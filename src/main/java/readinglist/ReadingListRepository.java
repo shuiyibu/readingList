75903a320352091884033ff5a0a614fe371f61c2
shuiyibu <296017297@qq.com>
2017-11-17 14:11:49 +0800

    项目提交

---
+++ src/main/java/readinglist/ReadingListRepository.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package readinglist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+/**
+ * @author Dylan Lang
+ *
+ */
+
+public interface ReadingListRepository extends JpaRepository<Book,Long> {
+    List<Book> findByReader(String reader);
+}
